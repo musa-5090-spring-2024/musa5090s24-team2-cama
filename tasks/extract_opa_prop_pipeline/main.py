7a11e39b783889edb0d4e80a171dde00e26cf198
Timothy O <timoliver200@gmail.com>
2024-04-01 15:55:49 -0400

    [docs] upload raw data script & envs, main.py changed to check existing file structure before downloading data & successfully uploads data to raw data bucket (also added time statements to view time needed for later timeouts); for environment variables, separated buckets into raw_data and prep_data while also adding the project id to prevent 'storage.Client()' errors

--- tasks/extract_opa_prop_pipeline/main.py
+++ tasks/extract_opa_prop_pipeline/main.py
@@ -1,11 +1,13 @@
 import os
 import pathlib
 import requests
+import time # for timeout testing
 from google.cloud import storage
-import os
 from dotenv import load_dotenv
 from google.oauth2 import service_account
 import googleapiclient.discovery  # type: ignore
+from google.cloud import api_keys_v2
+from google.cloud.api_keys_v2 import Key
 
 
 load_dotenv()
@@ -20,7 +22,9 @@ def create_key(email: str) -> None:
         scopes=["https://www.googleapis.com/auth/cloud-platform"],
     )
 
-    service = googleapiclient.discovery.build("iam", "v1", credentials=credentials)
+    service = googleapiclient.discovery.build(
+        "iam", "v1", credentials=credentials
+                                              )
 
     key = (
         service.projects()
@@ -40,40 +44,107 @@ def create_key(email: str) -> None:
     if not key["disabled"]:
         print("Created json key")
 '''
+'''
+def create_api_key(project_id: str, suffix: str) -> Key:
+    """
+    Creates and restrict an API key. Add the suffix for uniqueness.
 
-if not (os.path.exists("keys/service-account-key.json")):
-    create_key(os.getenv('AUTH_EMAIL'))
+    TODO(Developer):
+    1. Before running this sample,
+      set up ADC as described in https://cloud.google.com/docs/authentication/external/set-up-adc
+    2. Make sure you have the necessary permission to create API keys.
+
+    Args:
+        project_id: Google Cloud project id.
+
+    Returns:
+        response: Returns the created API Key.
+    """
+    # Create the API Keys client.
+    client = api_keys_v2.ApiKeysClient()
+
+    key = api_keys_v2.Key()
+    key.display_name = f"My first API key - {suffix}"
+
+    # Initialize request and set arguments.
+    request = api_keys_v2.CreateKeyRequest()
+    request.parent = f"projects/{project_id}/locations/global"
+    request.key = key
+
+    # Make the request and wait for the operation to complete.
+    response = client.create_key(request=request).result()
+
+    print(f"Successfully created an API key: {response.name}")
+    # For authenticating with the API key, use the value in "response.key_string".
+    # To restrict the usage of this API key, use the value in "response.name".
+    return response
+'''
 
 
-def extract_phl_opa_properties():
+def check_google_auth():
+    if not (os.path.exists(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))):
+        path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+        built_path = ""
+        for level in path.rsplit("/", 1)[0].split("/"):
+            if not os.path.exists(os.path.join(built_path, level)):
+                built_path = os.path.join(built_path, level)
+                os.makedirs(built_path)
+        open(
+            os.getenv("GOOGLE_APPLICATION_CREDENTIALS"), "w", encoding='utf-8'
+            ).close()
+        # create_key(os.getenv('AUTH_EMAIL'))
+
+
+def download_phl_opa_properties(url: str, fname: str):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(fname, 'wb') as f:
+        f.write(response.content)
+
+    print(f'Downloaded {fname}')
+
+
+def extract_phl_opa_properties(download=False):
 
     print("Extracting PHL OPA Properties data...")
 
     # Download the OPA Properties data as a CSV
-    url = 'https://opendata-downloads.s3.amazonaws.com/opa_properties_public.csv'
+    url = 'https://opendata-downloads.s3.amazonaws.com/' + \
+        'opa_properties_public.csv'
     filename = DATA_DIR / 'phl_opa_properties.csv'
 
-    response = requests.get(url)
-    response.raise_for_status()
-
-    with open(filename, 'wb') as f:
-        f.write(response.content)
-
-    print(f'Downloaded {filename}')
-
+    if download:
+        start = time.time()
+        download_phl_opa_properties(url, filename)
+        end = time.time()
+        print(f"Time taken for download:\n\t{end-start} seconds.") # took 514 seconds in last trial
+        
     # Upload the downloaded file to cloud storage
-    BUCKET_NAME = os.getenv('DATA_LAKE_BUCKET')
+    BUCKET_NAME = os.getenv('RAW_DATA_LAKE_BUCKET')
     blobname = 'raw/phl_opa_properties/phl_opa_properties.csv'
 
-    storage_client = storage.Client()
+    storage_client = storage.Client(project=os.getenv("PROJECT_ID"))
     bucket = storage_client.bucket(BUCKET_NAME)
     blob = bucket.blob(blobname)
-    blob.upload_from_filename(filename)
+    start = time.time()
+    blob.upload_from_filename(filename, timeout=(60*60))
+    end = time.time()
+    print(f"Time taken for upload:\n\t{end-start} seconds.") # took 47 minutes to successfully upload
 
     print(f'Uploaded {blobname} to {BUCKET_NAME}')
 
-    return "Downloaded and uploaded gs://{BUCKET_NAME}/{blobname} successfully"
+    return "Uploaded to gs://{BUCKET_NAME}/{blobname} successfully"
 
 
 if __name__ == "__main__":
-    extract_phl_opa_properties()
+    # check_google_auth()
+
+    # check if file exists before downloading        
+    extract_phl_opa_properties(
+        True if not (os.path.exists(
+            DATA_DIR / 'phl_opa_properties.csv'
+            )
+            ) else False)
+
+    # create_api_key("musa509s24-team2","")
