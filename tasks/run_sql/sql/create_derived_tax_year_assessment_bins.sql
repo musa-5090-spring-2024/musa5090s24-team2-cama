32fc3ed03867dfa810b04bb7d668349254808208
Sijia Zhang <126390570+jiajiabao01@users.noreply.github.com>
2024-04-10 12:19:07 -0400

    Add files via upload
    #Task4: Add SQL query for creating the table of derived_tax_year_assessment_bins

---
+++ tasks/run_sql/sql/create_derived_tax_year_assessment_bins.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE TABLE derived.tax_year_assessment_bins AS
+SELECT 
+    year AS tax_year,
+    FLOOR(market_value / 10000) * 10000 AS lower_bound,
+    FLOOR(market_value / 10000) * 10000 + 10000 AS upper_bound,
+    COUNT(*) AS property_count
+FROM 
+    source.opa_assessments
+GROUP BY 
+    year,lower_bound, upper_bound
+ORDER BY 
+    lower_bound;
