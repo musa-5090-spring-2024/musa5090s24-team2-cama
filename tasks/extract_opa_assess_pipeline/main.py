089e59ba94a70a1723eaae35f834ed94bf0ab96a
Richard Barad <richard.barad@gmail.com>
2024-04-17 13:24:56 -0400

    Push work on phl_assessment_pipeline
    Push work on the PHL assessment Pipeline

---
+++ tasks/extract_opa_assess_pipeline/main.py
@@ -0,0 +1,38 @@
+import requests
+import pathlib
+from google.cloud import storage
+from dotenv import load_dotenv
+import os
+import time
+
+load_dotenv(".env")
+DATA_DIR = pathlib.Path(__file__).parent
+
+def extract_opa_assess_pipeline():
+    url = 'https://opendata-downloads.s3.amazonaws.com/assessments.csv'
+    r = requests.get(url)
+    raw_data_folder = DATA_DIR / 'raw_data'
+    if not os.path.exists(raw_data_folder):
+        os.mkdir(raw_data_folder)
+    filename = DATA_DIR / 'raw_data/opa_assessments.csv'
+    with open(filename, "wb") as f:
+        for line in r.iter_lines():
+            f.write(line+'\n'.encode())
+    print('Writing File Complete')
+    # Upload the downloaded file to cloud storage
+    BUCKET_NAME = os.getenv('PREP_DATA_LAKE_BUCKET')
+    blobname = 'opa_assessments/assessments.csv'
+    print(os.getcwd())
+    storage_client = storage.Client(project=os.getenv("PROJECT_ID"))
+    bucket = storage_client.bucket(BUCKET_NAME)
+    blob = bucket.blob(blobname)
+    print("Starting file upload.")
+    start = time.time()
+    blob.upload_from_filename(filename, timeout=(60*3))
+    end = time.time()
+    print(f"Time taken for upload:\n\t{end-start} seconds.")  
+    # took 47 minutes to successfully upload
+    print(f'Uploaded {blobname} to {BUCKET_NAME}')
+    return f"Uploaded to gs://{BUCKET_NAME}/{blobname} successfully"
+
+extract_opa_assess_pipeline()
