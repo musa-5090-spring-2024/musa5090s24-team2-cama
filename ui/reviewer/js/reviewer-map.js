e90c27c01b9171e931a5a977da1ca3e1f856d15d
junyi2022 <junyi2022@berkeley.edu>
2024-04-18 12:09:06 -0400

    finish radio button

--- ui/reviewer/js/reviewer-map.js
+++ ui/reviewer/js/reviewer-map.js
@@ -1,5 +1,89 @@
 const url = 'https://storage.googleapis.com/musa5090s24_team02_public/tiles/properties/{z}/{x}/{y}.pbf';
 
+const assessRadio = document.getElementById("assessRadio");
+const predictRadio = document.getElementById("predictRadio");
+
+let styleProperty = 'current_assessed_value';
+
+function getMapStyle(styleProperty) {
+  const styleOptions = {
+    // A plain set of L.Path options.
+    'property_tile_info': (properties) => {
+
+      if (properties[styleProperty] < 160000) {
+        return {
+          weight: 0,
+          fillColor: '#5176E1',
+          fillOpacity: 1,
+          fill: true,
+        };
+      }
+
+      if (properties[styleProperty] < 250000 && properties[styleProperty] >= 160000) {
+        return {
+          weight: 0,
+          fillColor: '#6BA9E1',
+          fillOpacity: 1,
+          fill: true,
+        };
+      }
+
+      if (properties[styleProperty] < 400000 && properties[styleProperty] >= 250000) {
+        return {
+          weight: 0,
+          fillColor: '#98DBD6',
+          fillOpacity: 1,
+          fill: true,
+        };
+      }
+
+      if (properties[styleProperty] < 1000000 && properties[styleProperty] >= 400000) {
+        return {
+          weight: 0,
+          fillColor: '#E0B853',
+          fillOpacity: 1,
+          fill: true,
+        };
+      }
+
+      if (properties[styleProperty] < 4500000 && properties[styleProperty] >= 1000000) {
+        return {
+          weight: 0,
+          fillColor: '#E18A5B',
+          fillOpacity: 1,
+          fill: true,
+        };
+      }
+
+      else {
+        return {
+          weight: 0,
+          fillColor: '#E1544F',
+          fillOpacity: 1,
+          fill: true,
+        };
+      }
+    },
+  };
+
+  return styleOptions;
+}
+
+
+function handleRadioSelection(vectordata) {
+  assessRadio.addEventListener('click', () => {
+    styleProperty = 'tax_year_assessed_value';
+    console.log('clicked assess radio');
+    vectordata.redraw();
+  });
+
+  predictRadio.addEventListener('click', () => {
+    styleProperty = 'current_assessed_value';
+    console.log('clicked assess radio');
+    vectordata.redraw();
+  });
+}
+
 function initializeMap() {
   const map = L.map('map', { zoomSnap: 0, maxZoom: 20, minZoom: 12 }).setView([39.99, -75.15], 12); // zoomSnap 0 make the zoom level to real number
   const baseTileLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
@@ -10,74 +94,20 @@ function initializeMap() {
   const vectorTileOptions = {
     rendererFactory: L.canvas.tile,
     attribution: 'Something',
-    vectorTileLayerStyles: {
-      // A plain set of L.Path options.
-      'property_tile_info': function (properties) {
-
-        if (properties.current_assessed_value < 160000) {
-          return {
-            weight: 0,
-            fillColor: '#5176E1',
-            fillOpacity: 1,
-            fill: true,
-          };
-        }
-
-        if (properties.current_assessed_value < 250000 && properties.current_assessed_value >= 160000) {
-          return {
-            weight: 0,
-            fillColor: '#6BA9E1',
-            fillOpacity: 1,
-            fill: true,
-          };
-        }
-
-        if (properties.current_assessed_value < 400000 && properties.current_assessed_value >= 250000) {
-          return {
-            weight: 0,
-            fillColor: '#98DBD6',
-            fillOpacity: 1,
-            fill: true,
-          };
-        }
-
-        if (properties.current_assessed_value < 1000000 && properties.current_assessed_value >= 400000) {
-          return {
-            weight: 0,
-            fillColor: '#E0B853',
-            fillOpacity: 1,
-            fill: true,
-          };
-        }
-
-        if (properties.current_assessed_value < 4500000 && properties.current_assessed_value >= 1000000) {
-          return {
-            weight: 0,
-            fillColor: '#E18A5B',
-            fillOpacity: 1,
-            fill: true,
-          };
-        }
-
-        else {
-          return {
-            weight: 0,
-            fillColor: '#E1544F',
-            fillOpacity: 1,
-            fill: true,
-          };
-        }
-      },
-    },
+    vectorTileLayerStyles: getMapStyle(styleProperty),
   };
 
   const vectordata = L.vectorGrid.protobuf(url, vectorTileOptions).addTo(map);
   window.vectordata = vectordata;
 
+  handleRadioSelection(vectordata);
+
     //cross origin resource sharing (CORS)
 
   return map;
 }
+
+
 
 // vectordata.redraw();
 
